997035c209d84932ffcbd033bbb454412e28c91c
99duuk <99duuk@gmail.com>
2024-03-18 19:31:19 +0900

    메인/ 회원가입 & 로그인 기능 추가, DBConfig 삭제

--- BitBox/src/db/dao/LoginDao.java
+++ BitBox/src/db/dao/LoginDao.java
@@ -3,43 +3,94 @@ package db.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import db.DBConnection;
-import db.DBConnection.DBClose;
+import DTO.User;
 
 public class LoginDao {
 	Connection conn = null;
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
+
+	public static int joinUser(User user) throws Exception {
+		int result = 0;
 	
-	public boolean isMemberExists (String userId) {
-		String sql = "SELECT * FROM MEMBER WHERE USERID = ?";
-		try {
-			// db연결하기
-			conn = DBConnection.getConnection();
-			// sql 받을 상태 준비
-			pstmt = conn.prepareStatement(sql);
-			// ?에 값 넣기
-			pstmt.setString(1, userId);
-			// executeQuery로 쿼리실행 후 rs로 결과값 받기
-			rs = pstmt.executeQuery();
-			
-			if (rs.next()) {
-				System.out.println(rs.getString(1));
-				return rs.getString(1).length() > 0;
-			}
-			
-			
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			e.getStackTrace();
-		} finally {
-			DBClose.close(conn, pstmt, rs);
+		// 동일한 유저가 이미 존재하는지 확인 
+		if(isUserExists(user.getUser_id())) {
+			result = 2; //이미 존재 
+			return result;
 		}
 		
-		return false; // 회원존재x
+		// SQL 쿼리문 작성
+				String sql = "INSERT INTO user (user_id, user_pw) VALUES (?, ?)";
+				try (PreparedStatement pstmt = DBConnection.getConnection().prepareStatement(sql)) {
+					// PreparedStatement에 값 설정
+					pstmt.setString(1, user.getUser_id());
+					pstmt.setString(2, user.getUser_pw());
+
+					// 쿼리 실행 및 결과값 반환
+					result = pstmt.executeUpdate();
+				} catch (Exception e) {
+					// 예외 발생 시 오류 출력
+					e.printStackTrace();
+				}
+				// 가입 성공 여부 반환
+				return result;
+			}
+
+	
+	public static int loginUser(User user) throws Exception {
+		// 초기값 설정
+		int userId = 0;
+
+		// SQL 쿼리문 작성
+		String sql = "SELECT user_id FROM user WHERE user_id = ? AND user_pw = ?";
+		try (PreparedStatement pstmt = DBConnection.getConnection().prepareStatement(sql)) {
+			// PreparedStatement에 값 설정
+			pstmt.setString(1, user.getUser_id());
+			pstmt.setString(2, user.getUser_pw());
+
+			// 쿼리 실행 및 결과값 처리
+			ResultSet resultSet = pstmt.executeQuery();
+
+			// 결과가 존재하는 경우
+			if (resultSet.next()) {
+				// 로그인 성공 시 유저 ID 설정
+				userId = resultSet.getInt("user_id");
+			}
+		} catch (Exception e) {
+			// 예외 발생 시 오류 출력
+			e.printStackTrace();
+		}
+
+		// 로그인 결과 출력 후 유저 ID 반환
+		System.out.printf("\n\n<<  %s  >>\n\n", userId == 0 ? "로그인에 실패했습니다. 다시 시도해 주세요." : "로그인에 성공했습니다.");
+		return userId;
 	}
+	
 
+	public static boolean isUserExists(String user_id) {
+
+		String sql = "SELECT COUNT(*) FROM user WHERE user_id = ?";
+
+		try (PreparedStatement pstmt = DBConnection.getConnection().prepareStatement(sql)) {
+
+			pstmt.setString(1, user_id);
+
+			ResultSet resultSet = pstmt.executeQuery();
+			resultSet.next();
+			int count = resultSet.getInt(1);
+
+			return count > 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+//		finally {
+//	        // 데이터베이스 연결 닫기
+//	        DBClose.close(conn, pstmt, null);
+//	    }
+
+		return false;
+	}
 }
-
--- BitBox/src/server/MainServer.java
+++ BitBox/src/server/MainServer.java
@@ -7,90 +7,132 @@ import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 import org.json.JSONObject;
 
+import DTO.User;
 import db.dao.LoginDao;
 
 public class MainServer extends WebSocketServer {
-	public static void main(String[] args) {
-//		String host = "192.168.0.20";
-		String host = "127.0.0.1";
-		final int PORT = 9000;
-		
-		WebSocketServer server = new MainServer(new InetSocketAddress(host, PORT));
-		server.run();
-	}
-	
-	public MainServer(InetSocketAddress inetAddr) {
-		super(inetAddr);
-	}
+    public static void main(String[] args) {
+        String host = "127.0.0.1";
+        final int PORT = 1111;
 
-	@Override
-	public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-		System.out.println(conn + "연결이 끊겼습니다.");
-		
-	}
+        WebSocketServer server = new MainServer(new InetSocketAddress(host, PORT));
+        server.run();
+    }
 
-	@Override
-	public void onError(WebSocket conn, Exception ex) {
-		System.out.println(ex.getMessage());
-		ex.getStackTrace();
-	}
+    public MainServer(InetSocketAddress inetAddr) {
+        super(inetAddr);
+    }
 
-	@Override
-	public void onMessage(WebSocket conn, String message) {
-		System.out.println( "님이 들어왔습니다" + message);
-		
-		JSONObject msgObj = new JSONObject(message);
-		// 패킷 종류를 구분할 수 있는 명령어를 제일 먼저 추출
-		String cmd = msgObj.getString("cmd");
-		
-		if(cmd.equals("login")) {
-			String id = msgObj.getString("id");
-			String pass = msgObj.getString("pass");
-			System.out.printf("로그인 id: %s   pass: %s\n", id, pass);
-			
-			/* DB에서 해당 id, pass가 있는 지 확인하는 작업 추가
-			 * */
-			LoginDao loginDao = new LoginDao();
-			if(loginDao.isMemberExists(id)) {
-				System.out.println("성공~");
-			} else {
-				System.out.println("실패....");
-			}
-			
-			
-			JSONObject ackObj = new JSONObject();
-			ackObj.put("cmd", "login");
-			ackObj.put("result", "ok");
-			conn.send(ackObj.toString());   // json문자열로 변환되어서 클라이언트한테 전송됨
-		}else if(cmd.equals("allchat")) {
-			String id = msgObj.getString("id");
-			String msg = msgObj.getString("msg");
-			System.out.printf("id: %s	msg: %s\n", id, msg);
-			
-			// 전체 접속자한테 브로드캐스팅
-			for(WebSocket con : this.getConnections()) {
-				if(conn != con) {
-					con.send(message);
-				}
-			}
-		}
-	}
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        System.out.println(conn + "연결이 끊겼습니다.");
 
-	@Override
-	public void onOpen(WebSocket conn, ClientHandshake handshake) {
-		String hostIp = conn.getRemoteSocketAddress().getAddress().getHostAddress().toString();
-		System.out.println(hostIp + " 님이 연결되었습니다."); // 누가 연결 했는지
-		
-		JSONObject ackObj = new JSONObject();
-		ackObj.put("cmd", "connect");
-		ackObj.put("result", "잘 연결되었습니다.");
-		conn.send(ackObj.toString()); // 클라이언트한테 메시지 보내기
-	}
+    }
 
-	@Override
-	public void onStart() {
-		// 서버 연결
-		System.out.println("Server#성공적#해냈음");
-	}
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        System.out.println(ex.getMessage());
+        ex.getStackTrace();
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        System.out.println("님이 들어왔습니다" + message);
+
+        JSONObject msgObj = new JSONObject(message);
+        // 패킷 종류를 구분할 수 있는 명령어를 제일 먼저 추출
+        String cmd = msgObj.getString("cmd");
+
+        if (cmd.equals("join")) {
+            String id = msgObj.getString("id");
+            String pass = msgObj.getString("pass");
+            System.out.printf("회원가입 id: %s   pass: %s\n", id, pass);
+
+            // 회원가입 시도
+            try {
+                User user = new User();
+                user.setUser_id(id);
+                user.setUser_pw(pass);
+
+                int result = LoginDao.joinUser(user); // 회원가입 시도
+
+                JSONObject ackObj = new JSONObject();
+                ackObj.put("cmd", "join");
+
+                if (result == 1) {
+                    // 회원가입 성공 시
+                    ackObj.put("result", "ok");
+                } else if (result == 2) {
+                    // 이미 존재하는 사용자일 경우
+                    ackObj.put("result", "exist");
+                } else {
+                    // 그 외의 경우 (가입 실패 등)
+                    ackObj.put("result", "fail");
+                }
+
+                conn.send(ackObj.toString()); // 클라이언트에게 응답 전송
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (cmd.equals("login")) {
+            String id = msgObj.getString("id");
+            String pass = msgObj.getString("pass");
+            System.out.printf("로그인 id: %s   pass: %s\n", id, pass);
+
+            // 로그인 시도
+            try {
+                User user = new User();
+                user.setUser_id(id);
+                user.setUser_pw(pass);
+
+                int userId = LoginDao.loginUser(user);
+
+                JSONObject ackObj = new JSONObject();
+                ackObj.put("cmd", "login");
+
+                if (userId != 0) {
+                    // 로그인 성공 시
+                    ackObj.put("result", "ok");
+                    ackObj.put("userId", userId);
+                } else {
+                    // 로그인 실패 시
+                    ackObj.put("result", "fail");
+                }
+
+                conn.send(ackObj.toString()); // 클라이언트에게 응답 전송
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else if (cmd.equals("allchat")) {
+            String id = msgObj.getString("id");
+            String msg = msgObj.getString("msg");
+            System.out.printf("id: %s   msg: %s\n", id, msg);
+
+            // 전체 접속자한테 브로드캐스팅
+            for (WebSocket con : this.getConnections()) {
+                if (conn != con) {
+                    con.send(message);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        String hostIp = conn.getRemoteSocketAddress().getAddress().getHostAddress().toString();
+        System.out.println(hostIp + " 님이 연결되었습니다."); // 누가 연결 했는지
+
+        JSONObject ackObj = new JSONObject();
+        ackObj.put("cmd", "connect");
+        ackObj.put("result", "연결 성공 !!!");
+        conn.send(ackObj.toString()); // 클라이언트한테 메시지 보내기
+    }
+
+    @Override
+    public void onStart() {
+        // 서버 연결
+        System.out.println("BitBox 서버 실행 !!!");
+    }
 
 }
