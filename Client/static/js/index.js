0daceb40c747c33d374d357908c1ad2f25343dc5
jidefine <youjio2000@gmail.com>
2024-03-20 14:59:21 +0900

    로그인/오류 수정 중

--- Client/static/js/index.js
+++ Client/static/js/index.js
@@ -1,9 +1,9 @@
-//팝업 띄우기
-function openPop() {
-    document.getElementById("popup_layer").style.display = "block";
-}
+// //팝업 띄우기
+// function openPop() {
+//     document.getElementById("popup_layer").style.display = "block";
+// }
 
-//팝업 닫기
-function closePop() {
-    document.getElementById("popup_layer").style.display = "none";
-}
+// //팝업 닫기
+// function closePop() {
+//     document.getElementById("popup_layer").style.display = "none";
+// }
