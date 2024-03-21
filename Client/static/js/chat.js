ea58bc8d58e4266715aaa1e7508d31d5cd8e6f07
Sophie-Ahn <sgdream95@gmail.com>
2024-03-21 17:48:09 +0900

    채팅/필요없는 코드 삭제

--- Client/static/js/chat.js
+++ Client/static/js/chat.js
@@ -1,18 +1,6 @@
-let userId = ""; // id 보관용
 let cnt = 0;
+let rommname = "";
 
-const sendLogIn = function () {
-  const id = document.querySelector("#idInput").value;
-  const pass = document.querySelector("#passInput").value;
-  userId = id; // 나중에 msg전송 때 사용하기 위해
-  const packet = {
-    cmd: "login",
-    id: id,
-    pass: pass,
-  };
-  const jsonStr = JSON.stringify(packet); // js객체 -> json문자열
-  sendMessage(jsonStr);
-};
 const sendChatMsg = function () {
   const message = document.querySelector("#messageInput").value;
   const packet = {
@@ -27,19 +15,20 @@ const sendChatMsg = function () {
 };
 
 const newChatMsg = function () {
-  const packet = {
-    cmd: "newchat",
-    room: cnt,
-  };
-  const jsonStr = JSON.stringify(packet); // js객체 -> json문자열
-  sendMessage(jsonStr);
-
   const chatAdd = document.querySelector(".chat_add");
   const addDiv = document.createElement("div");
   chatAdd.after(addDiv);
   addDiv.classList.add("chat_list");
-  addDiv.id = 'chat_list' + cnt;
+  roomname = addDiv.id = 'chat_list' + cnt;
   addDiv.textContent = "채팅방" + cnt++;
+
+  const packet = {
+    cmd: "newchat",
+    room: cnt,
+    name: roomname,
+  };
+  const jsonStr = JSON.stringify(packet); // js객체 -> json문자열
+  sendMessage(jsonStr);
 };
 
 const enterChatMsg = function () {
@@ -63,15 +52,11 @@ function scrollUl() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const btnLogIn = document.querySelector("#btnLogIn");
   const btnChagMsg = document.querySelector("#btnChatMsg");
   const messageInput = document.querySelector("#messageInput");
   let chatForm = document.querySelector("#messages");
   const newChatBtn = document.querySelector("#chat_add_btn");
 
-
-
-  btnLogIn.addEventListener("click", sendLogIn);
   btnChagMsg.addEventListener("click", function (e) {
     e.preventDefault();
     sendChatMsg(e);
