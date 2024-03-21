a1fb981de222987e55cc3da08fc34b5b4475990a
jidefine <youjio2000@gmail.com>
2024-03-21 13:58:03 +0900

    로그인/모든 페이지에 로그인 팝업창 적용

--- Client/static/js/gnb.js
+++ Client/static/js/gnb.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     window.location.href = 'D:/BITBOX//Client/board/boardRead.html';
                     break;
                 case '지도':
-                    window.location.href = 'D:/BITBOX//Client/map/map.html';
+                    window.location.href = 'D:/BITBOX//Client/map/map_pop.html';
                     break;
                 case '채팅':
                     window.location.href = 'D:/BITBOX//Client/chat/chat.html';
