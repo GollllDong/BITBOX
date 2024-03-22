f05ad625c37aa860e2348e6438c23a18008a76a4
99duuk <99duuk@gmail.com>
2024-03-22 17:33:53 +0900

    Client / 세션스토리지 추가

--- Client/static/js/socket-client.js
+++ Client/static/js/socket-client.js
@@ -33,29 +33,7 @@ socket.onmessage = function (e) {
     displayMessage("#messages", log_msg);
     displayPacketMessage("#messages", e.data);
 
-
-    // // <------------------------------세션 스토리지 추가-------------------------------->
-    // const msgObj = JSON.parse(e.data);
-    // if (msgObj.cmd === "login" && msgObj.result === "ok") {
-    //     // 로그인 성공 시 세션 스토리지에 'user_id', 'id_idx' 저장
-    //     sessionStorage.setItem("user_id", msgObj.data.user_id);
-    //     sessionStorage.setItem("id_idx", msgObj.data.id_idx);
-    //     // 로그인 후에 반환된 user_id와 id_idx를 로그인한 웹 세션에 고정시키고
-    //     // 기능을 서버에 요청할 때마다 cmd와 user_id를 보낼 수 있게 
-    // }
 };
-// sessionStorage.getItem("id_idx", msgObj.data.id_idx);
-// //서 - 클 연결간 == 세션
-// setItem 으로 저장했고
-// user_id = getItem 변수에 넣어주고
-// // 
-// window 객체(모든애들이 공유라)에 전역변수 var 타입으로 선언하면 
-// 아무대서나 쓸 수 있음 
-
-// nimoh
-
-// 전역변수 남발 X 
-// 필요할 때만 뽑아쓰도록..!(getItem)
 
 
 
@@ -65,6 +43,19 @@ const sendMessage = function (message) {
     const log_msg = `클라이언트 => 서버 ${message}`;
     displayMessage("#messages", log_msg);
 };
+
+
+
+const loginSuccess = function (msgObj) {
+    // 로그인 후 UI 변경
+    console.log("로그인 성공!");
+    sessionStorage.setItem({SESSION_KEY}, "true");
+    // 로그인 성공 시 세션 스토리지에 'user_id', 'id_idx' 저장
+    sessionStorage.setItem("user_id", msgObj.user_id);
+    sessionStorage.setItem("id_idx", msgObj.id_idx);
+};
+
+
 
 // 이벤트 로그 출력
 const displayMessage = function ($parentSelector, log_msg, kind_log = 0) {
@@ -78,16 +69,17 @@ const displayMessage = function ($parentSelector, log_msg, kind_log = 0) {
         parentElem.appendChild(childElem);
     }
 };
+
 // 통신 패킷 출력
 const displayPacketMessage = function ($parentSelector, message) {
     // 이 요소 아래에 메시지 요소를 추가
     const parentElem = document.querySelector($parentSelector);
-
     // json문자열 -> js 객체로 변환
     const msgObj = JSON.parse(message);
 
     let msg = "";
     let idMsg = "";
+    let loginInfo = "";
     switch (msgObj.cmd) {
         case "connect":
             msg = msgObj.result;
@@ -95,6 +87,9 @@ const displayPacketMessage = function ($parentSelector, message) {
         case "signup":
             if (msgObj.result === "ok") {
                 msg = "회원가입 성공";
+                window.location.href = '../../index.html';
+                // location.href = '../../index.html';
+                //페이지 이동 & openpopup
             } else if (msgObj.result === "exist") {
                 msg = "이미 존재하는 사용자입니다.";
             } else {
@@ -102,14 +97,16 @@ const displayPacketMessage = function ($parentSelector, message) {
             }
             break;
         case "login":
-            msg = msgObj.result === "ok" ? "로그인 성공" : "로그인 실패";
+            if (msgObj.result === "ok") {
+                loginSuccess(msgObj);
+                closePop();
+            } else {
+                alert("로그인 실패");
+            }
             break;
-
-        // <-----------------------------insertpost 추가-------------------------------->
         case "insertpost":
             msg = msgObj.result === "ok" ? "게시물 작성 성공" : "게시물 작성 실패";
             break;
-        // <----------------------------updatepost 추가-------------------------------->
         case "updatepost":
             msg = msgObj.result === "ok" ? "게시물 수정 성공" : "게시물 수정 실패";
             break;
@@ -128,20 +125,25 @@ const displayPacketMessage = function ($parentSelector, message) {
             break;
     }
 
+    // post 조회 뿌려줄 
+    // const childIdElem = document.createElement(".table tbody tr"); 작성 필요
+
     // const childElem = document.createElement("h3");
     // childElem.textContent = msg;
     // parentElem.appendChild(childElem);
 
-    // 채팅부분
-    const childIdElem = document.createElement("p");
-    const childSpanElem = document.createElement("span");
-    childIdElem.textContent = idMsg;
-    childSpanElem.textContent = msg;
+    
     // if (childSpanElem.textContent != "" && childIdElem.textContent != "") {
-       
+    //    // 채팅부분
+    // const childIdElem = document.createElement("p");
+    // const childSpanElem = document.createElement("span");
+    // childIdElem.textContent = idMsg;
+    // childSpanElem.textContent = msg;
+
+    // childIdElem.classList.add("chat_id");
+    // childSpanElem.classList.add("chat_msg");
+    // parentElem.appendChild(childIdElem);
+    // parentElem.appendChild(childSpanElem);
     // }
-    childIdElem.classList.add("chat_id");
-    childSpanElem.classList.add("chat_msg");
-    parentElem.appendChild(childIdElem);
-    parentElem.appendChild(childSpanElem);
+    
 };
