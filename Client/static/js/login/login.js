8b1c83855ee1de89bdacb8f36e072420ff0fbb53
Sophie-Ahn <sgdream95@gmail.com>
2024-03-24 01:01:13 +0900

    지도/경로수정 및 코드 수정

--- Client/static/js/login/login.js
+++ Client/static/js/login/login.js
@@ -1,6 +1,3 @@
-
-
-
 //팝업 띄우기
 function openPop() {
     document.getElementById("popup_layer").style.display = "block";
@@ -12,36 +9,26 @@ function closePop() {
     document.getElementById("popup_layer").style.display = "none";
 }
 
-// // 로그아웃 시 세션 스토리지에서 로그인 상태 삭제
-// const logout = function () {
-//     sessionStorage.removeItem(SESSION_KEY);
-//     // 나머지 로그아웃 처리 코드...
-// };
 
+const sendSignup = function () {
+    const user_id = document.querySelector("#idInput").value;
+    const user_pw = document.querySelector("#passInput").value;
+    const user_name = document.querySelector("#name").value;
+    const course_id = document.querySelector("#course").value;
+    const course_code = document.querySelector("#code").value;
 
+    const packet = {
+        cmd: "signup",
+        user_id: user_id,
+        user_pw: user_pw,
+        user_name: user_name,
+        course_id: course_id,
+        course_code: course_code
 
-// const SESSION_KEY = "user_login";
-
-// console.log(document.getElementById("loginBefore"));
-
-// const isLoggedIn = sessionStorage.getItem(SESSION_KEY) === "true";
-// // 페이지 로드 시 로그인 상태에 따라 UI 변경
-// window.onload = function () {
-//     const loginBefore = document.getElementById("loginBefore");
-//     const loginAfter = document.getElementById("loginAfter");
-
-//     if (sessionStorage.getItem("user_id") !== null) {
-//         console.log(isLoggedIn);
-//         loginBefore.style.display = "none";
-//         loginAfter.style.display = "block";
-//         // } else {
-//         //     console.log(isLoggedIn +"$$$$$$$");
-//         //     loginBefore.style.display = "block";
-//         //     loginAfter.style.display = "none";
-//     }
-// };
-
-
+    };
+    const jsonStr = JSON.stringify(packet); // js객체 -> json문자열
+    sendMessage(jsonStr);
+};
 
 
 const sendLogIn = function () {
@@ -63,23 +50,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const login_error = document.getElementById("login_error");
 
     if (sessionStorage.getItem("user_id") !== null) {
-        
         loginBefore.style.display = "none";
         loginAfter.style.display = "block";
     }
 
     const login_btn = document.querySelector('#login_btn');
+    if (login_btn) login_btn.addEventListener('click', sendLogIn);
 
-    login_btn.addEventListener('click', sendLogIn);
-});
-
-// 비밀번호 보이기, 감추기
-document.addEventListener('DOMContentLoaded', function () {
-    const loginBtn = document.querySelector('#login_btn');
-    loginBtn.addEventListener('click', sendLogIn);
+    const createAcct_btn = document.querySelector("#createAcct_btn");
+    if (createAcct_btn) createAcct_btn.addEventListener("click", sendSignup);
 
     const showPasswordIcon = document.querySelector('.showPassword'); // 눈 아이콘을 가리키는 요소 선택
-    showPasswordIcon.addEventListener('click', function () {
+    if (showPasswordIcon) showPasswordIcon.addEventListener('click', function () {
         const passwordInput = document.querySelector('#passInput');
         const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
         passwordInput.setAttribute('type', type);
